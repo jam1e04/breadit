85adc2872156a9ec5404c81de88289fca0e23e87
zmzheng <jjamie66@qq.com>
2025-06-03 17:44:21 +0800

    homepage simply modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import { HomeIcon } from "lucide-react"
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <>
+      <h1 className='font-bold text-3xl md:text-4xl'>Your feed</h1>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6'>
+        {/* subreddit info */}
+        <div className='overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last'>
+          <div className='bg-emerald-100 px-6 py-4'>
+            <p className='font-semibold py-3 flex items-center gap-1.5'>
+              <HomeIcon className='h-4 w-4' />
+              Home
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
