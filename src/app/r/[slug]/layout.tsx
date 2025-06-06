c968b6f3ce3dd0b0dc34683611c5e46c6e6355b5
zmzheng <jjamie66@qq.com>
2025-06-06 09:00:56 +0800

    Subreddit detail page done

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,6 +1,7 @@
 import { getAuthSession } from "@/lib/auth"
 import { db } from "@/lib/db"
 import { notFound } from "next/navigation"
+import { format } from 'date-fns'
 
 const Layout = async({ children, params: { slug } }: {
     children: React.ReactNode
@@ -56,11 +57,32 @@ const Layout = async({ children, params: { slug } }: {
                 {/* info sidebar */}
                 <div className="hidden md:block overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
                     <div className="px-6 py-4">
-                        <p className="font-semibold py-3">About r/</p>
+                        <p className="font-semibold py-3">About r/{subreddit.name}</p>
                     </div>
 
                     <dl className="divide-y divide-gray-100 px-6 py-4 text-sm leading-6 bg-white">
-                        <div className="flex justify-between"></div>
+                        <div className="flex justify-between gap-x-4 py-3">
+                            <dt className="text-gray-500">Created</dt>
+                            <dd className="text-gray-700">
+                                <time dateTime={subreddit.createdAt.toDateString()}>
+                                    {format(subreddit.createdAt,'MMMM d, yyyy')}
+                                </time>
+                            </dd>
+                        </div>
+
+                        <div className="flex justify-between gap-x-4 py-3">
+                        <dt className="text-gray-500">Members</dt>
+                            <dd className="text-gray-700">
+                                <div className="text-gray-900">{memberCount}</div>
+                            </dd>
+                        </div>
+
+                        {subreddit.creatorId === session?.user.id ? (
+                            <div className="flex justify-between gap-x-4 py-3">
+                                <p className="text-gray-500">You created this community</p>
+                            </div>
+                        ) : null}
+                        
                     </dl>
                 </div>
             </div>
