e03ec592c72f250fa73eeca955a6c818e12eda33
zmzheng <jjamie66@qq.com>
2025-06-13 14:59:40 +0800

    voting for coments & replying to comments

--- src/components/CommentsSection.tsx
+++ src/components/CommentsSection.tsx
@@ -34,31 +34,68 @@ const CommentsSection = async ({ postId }: CommentsSectionProps) => {
       <CreateComment postId={postId} />
 
       <div className="flex flex-col gap-y-6 mt-4">
-        {comments
-          .filter((comment) => !comment.replyToId)
-          .map((topLevelComment) => {
-            //   每条评论总投票数
-            const topLevelCommentVotesAmt = topLevelComment.votes.reduce(
-              (acc, vote) => {
-                if (vote.type === "UP") return acc + 1;
-                if (vote.type === "DOWN") return acc - 1;
-                return acc;
-              },
-              0
-            );
-            // 找到用户本人的投票
-            const topLevelCommentVote = topLevelComment.votes.find(
-              (vote) => vote.userId === session?.user.id
-            );
+        {
+          //顶级评论
+          comments
+            .filter((comment) => !comment.replyToId)
+            .map((topLevelComment) => {
+              //   每条评论总投票数
+              const topLevelCommentVotesAmt = topLevelComment.votes.reduce(
+                (acc, vote) => {
+                  if (vote.type === "UP") return acc + 1;
+                  if (vote.type === "DOWN") return acc - 1;
+                  return acc;
+                },
+                0
+              );
+              // 找到用户本人的投票
+              const topLevelCommentVote = topLevelComment.votes.find(
+                (vote) => vote.userId === session?.user.id
+              );
 
-            return (
-              <div key={topLevelComment.id} className="flex flex-col">
-                <div className="mb-2">
-                  <PostComment comment={topLevelComment} />
+              return (
+                <div key={topLevelComment.id} className="flex flex-col">
+                  <div className="mb-2">
+                    <PostComment
+                      postId={postId}
+                      comment={topLevelComment}
+                      currentVote={topLevelCommentVote}
+                      votesAmt={topLevelCommentVotesAmt}
+                    />
+                  </div>
+
+                  {/* render replies */}
+                  {topLevelComment.replies
+                    .sort((a, b) => b.votes.length - a.votes.length)
+                    .map((reply) => {
+                      const replyVotesAmt = reply.votes.reduce((acc, vote) => {
+                        if (vote.type === "UP") return acc + 1;
+                        if (vote.type === "DOWN") return acc - 1;
+                        return acc;
+                      }, 0);
+                      // 找到用户本人的投票
+                      const replyVote = reply.votes.find(
+                        (vote) => vote.userId === session?.user.id
+                      );
+
+                      return (
+                        <div
+                          key={reply.id}
+                          className="ml-2 py-2 pl-4 border-l-2 border-zinc-200"
+                        >
+                          <PostComment
+                            comment={reply}
+                            currentVote={replyVote}
+                            votesAmt={replyVotesAmt}
+                            postId={postId}
+                          />
+                        </div>
+                      );
+                    })}
                 </div>
-              </div>
-            );
-          })}
+              );
+            })
+        }
       </div>
     </div>
   );
